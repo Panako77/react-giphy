4784473ca6004331da8f1ca10ff2e509f1b2edc1
Panako77 <celdavoip@gmail.com>
2021-10-05 19:37:56 +0200

    docs agregado

---
+++ docs/precache-manifest.e5ab3dbbcbb2d5ce4f0f744ebf0c5943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b54732cd58fc69d8b8e225bfa0edec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbf07543a53c0086dfbb",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "83431ddc88055c8287c7",
+    "url": "/static/css/main.168d3aca.chunk.css"
+  },
+  {
+    "revision": "bbf07543a53c0086dfbb",
+    "url": "/static/js/2.44d08183.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.44d08183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83431ddc88055c8287c7",
+    "url": "/static/js/main.c1e74d9a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
