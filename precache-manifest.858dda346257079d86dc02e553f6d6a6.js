714516b5d7797c714fb201bc3671b49e7c7af38c
Panako77 <celdavoip@gmail.com>
2021-10-05 20:09:19 +0200

    Updates

---
+++ precache-manifest.858dda346257079d86dc02e553f6d6a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67545f82bb461c51c41cb2b32e77beb7",
+    "url": "/react-giphy/index.html"
+  },
+  {
+    "revision": "bbf07543a53c0086dfbb",
+    "url": "/react-giphy/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "83431ddc88055c8287c7",
+    "url": "/react-giphy/static/css/main.168d3aca.chunk.css"
+  },
+  {
+    "revision": "bbf07543a53c0086dfbb",
+    "url": "/react-giphy/static/js/2.44d08183.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-giphy/static/js/2.44d08183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83431ddc88055c8287c7",
+    "url": "/react-giphy/static/js/main.c1e74d9a.chunk.js"
+  },
+  {
+    "revision": "946b295e915842d9c3a6",
+    "url": "/react-giphy/static/js/runtime-main.d8c90f38.js"
+  }
+]);
